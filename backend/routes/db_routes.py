756da321817d386505de412e232266781ac0e6a3
heights-m <ryupii.24@gmail.com>
2025-10-06 23:55:33 +0900

    update backend to connect to DB on startup

--- backend/routes/db_routes.py
+++ backend/routes/db_routes.py
@@ -2,15 +2,3 @@ from fastapi import APIRouter
 from db.atlas_client import AtlasClient
 
 router = APIRouter(prefix="/db", tags=["Database"])
-
-client = AtlasClient()
-
-@router.post("/ping")
-def ping():
-    client.ping()
-    return {"message": "ping'd"}
-
-@router.get("/getUsers")
-def get_users():
-    users = client.get_users()
-    return users
--- backend/server.py
+++ backend/server.py
@@ -3,8 +3,30 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.config import FRONTEND_ORIGIN
 from routes import db_routes, predict_routes
+from pymongo import AsyncMongoClient
+from dotenv import load_dotenv
+import os
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+#Load .env variables
+load_dotenv()
+ATLAS_URI = os.getenv('ATLAS_URI')
+
+@asynccontextmanager
+async def db_lifespan(app: FastAPI):
+    #Server Startup
+    app.mongodb_client = AsyncMongoClient(ATLAS_URI)
+    app.database = app.mongodb_client.get_database("unnamed_db")
+    ping_response = await app.database.command("ping")
+    if int(ping_response["ok"]) != 1:
+        raise Exception("Problem connecting to mongoDB cluster")
+    else:
+        print("Connected to mongoDB cluster")
+    yield
+    #Server Shutdown
+    await app.mongodb_client.close()
+
+app = FastAPI(lifespan=db_lifespan)
 
 app.add_middleware(
     CORSMiddleware,
