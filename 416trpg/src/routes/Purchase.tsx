30e18a9df6a5dc7ce2aabab44a933a3ce79c4745
Yoonseong Kim <yoonseong.kim.1@stonybrook.edu>
2025-11-17 12:30:46 +0900

    Purchase Page Update

--- 416trpg/src/routes/Purchase.tsx
+++ 416trpg/src/routes/Purchase.tsx
@@ -1,6 +1,33 @@
 import './Purchase.css'
+import { ToastContainer, toast } from "react-toastify";
+
+// const apiUrl = import.meta.env.VITE_API_URL
 
 function PurchasePage () {
+
+    const purchaseMonthly = () => {
+        // TODO: Backend glorp, if successful run toast.success else run toast.error
+        toast.success("Purchase Monthly Successful")
+        // toast.error("Purchase Monthly Failed")
+    }
+
+    const purchase50 = () => {
+        // TODO: Backend glorp, if successful run toast.success else run toast.error
+        toast.success("Purchase Token Successful")
+        // toast.error("Token Purchase Failed")
+    }
+
+    const purchase100 = () => {
+        // TODO: Backend glorp, if successful run toast.success else run toast.error
+        toast.success("Purchase Token Successful")
+        // toast.error("Token Purchase Failed")
+    }
+
+    const purchase200 = () => {
+        // TODO: Backend glorp, if successful run toast.success else run toast.error
+        toast.success("Purchase Token Successful")
+        // toast.error("Token Purchase Failed")
+    }
 
     return (
         <div className="purchasetokenpage container-fluid py-5 d-flex justify-content-center align-items-center">
@@ -27,7 +54,7 @@ function PurchasePage () {
                         <div className="purchasepage-feature-card">
                             <span className='text-start'><b>Price: </b>$20</span>
                             <hr />                
-                            <button className='btn purchasepage-glow-btn mt-4'>Buy Now</button>
+                            <button className='btn purchasepage-glow-btn mt-4' onClick={purchaseMonthly}>Buy Now</button>
                         </div>
                     </div>
                     
@@ -44,25 +71,29 @@ function PurchasePage () {
                             <hr />
                             <span className='text-start'><b>Price: </b>$7</span>  
                             <hr />
-                            <button className='btn purchasepage-glow-btn mt-4'>Buy Now</button>
+                            <button className='btn purchasepage-glow-btn mt-4' onClick={purchase50}>Buy Now</button>
                         </div>
                         <div className="purchasepage-feature-card">
                             <h3 className='mb-3'>100 Tokens</h3>
                             <hr />
                             <span className='text-start'><b>Price: </b>$10</span>  
                             <hr />
-                            <button className='btn purchasepage-glow-btn mt-4'>Buy Now</button>
+                            <button className='btn purchasepage-glow-btn mt-4' onClick={purchase100}>Buy Now</button>
                         </div>
                         <div className="purchasepage-feature-card">
                             <h3 className='mb-3'>200 Tokens</h3>
                             <hr />
                             <span className='text-start'><b>Price: </b>$15</span>
                             <hr />                
-                            <button className='btn purchasepage-glow-btn mt-4'>Buy Now</button>
+                            <button className='btn purchasepage-glow-btn mt-4' onClick={purchase200}>Buy Now</button>
                         </div>
                     </div>
                 </div>
             </div>
+
+        {/* Toast container */}
+        <ToastContainer position="top-center" autoClose={2500} theme="colored" />
+
         </div>
     )
 
